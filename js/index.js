27225d501473ffebbf421898e4608a4aec7eb3a7
Peter O'Connor <poc275@gmail.com>
2019-04-06 21:58:25 +0100

    Tweening tweaked.

--- js/index.js
+++ js/index.js
@@ -1,13 +1,15 @@
 var controller = new ScrollMagic.Controller();
 
-var blockTween = new TweenMax.fromTo('#header', 1, {scaleY: 1}, {scaleY: 0.8});
+var headerShrinkTween = new TweenMax.fromTo('#header', 1, {scaleY: 1}, {scaleY: 0.25});
+// var navbarMoveTween = new TweenMax.to('#sticky-navbar', 1, {y: -100});
 
 new ScrollMagic.Scene({
         // duration: 100,
-        offset: 100
+        offset: 50     // start animation at this scroll position
         // triggerElement: '#sticky-navbar'
     })
-    .setTween(blockTween)
+    .setTween(headerShrinkTween)
+    // .setTween([headerShrinkTween, navbarMoveTween])
     // .addIndicators()
     // .setPin("#header")
     .addTo(controller);
