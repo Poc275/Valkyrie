351148def598f86aec0c8b4df3055743b4319d25
Peter O'Connor <poc275@gmail.com>
2021-02-21 10:26:28 +0000

    Makes carousel more prominent on home page.

--- js/index.js
+++ js/index.js
@@ -1,15 +1,15 @@
-var controller = new ScrollMagic.Controller();
+// var controller = new ScrollMagic.Controller();
 
-var headerShrinkTween = new TweenMax.fromTo('#header', 1, {scaleY: 1}, {scaleY: 0.25});
-// var navbarMoveTween = new TweenMax.to('#sticky-navbar', 1, {y: -100});
+// var headerShrinkTween = new TweenMax.fromTo('#header', 1, {scaleY: 1}, {scaleY: 0.25});
+// // var navbarMoveTween = new TweenMax.to('#sticky-navbar', 1, {y: -100});
 
-new ScrollMagic.Scene({
-        // duration: 100,
-        offset: 50     // start animation at this scroll position
-        // triggerElement: '#sticky-navbar'
-    })
-    .setTween(headerShrinkTween)
-    // .setTween([headerShrinkTween, navbarMoveTween])
-    // .addIndicators()
-    // .setPin("#header")
-    .addTo(controller);
+// new ScrollMagic.Scene({
+//         // duration: 100,
+//         offset: 50     // start animation at this scroll position
+//         // triggerElement: '#sticky-navbar'
+//     })
+//     .setTween(headerShrinkTween)
+//     // .setTween([headerShrinkTween, navbarMoveTween])
+//     // .addIndicators()
+//     // .setPin("#header")
+//     .addTo(controller);
